0ee5c7cc53e3b7dadd6fb2f4309e592418af75a7
Khanh Vo Hoang Ngoc <vokhanh2002vl1@gmail.com>
2023-06-11 21:27:43 +0700

    somethings update

--- src/server.js
+++ src/server.js
@@ -2,33 +2,29 @@ const path= require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
-const app = express()
+const app = express() // trả về đối tượng để xây dựng lại web 
 const port = 3000
-
 app.use(express.static(path.join(__dirname, 'public')))
-//HTTP logger
 
+// Using combined predefined format, http logger
+app.use(morgan("combined"));
 
-app.use(morgan('combined'))
 //template engine
 app.engine('hbs', handlebars.engine({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
+//req = request : chứa tất cả thông tin yêu cầu gửi đi(path, đường dẫn, phương thức, thông tin liên quan trong header) yêu cầu về phía server
+//res = response : server nhận request -> xử lý (trả về client các dữ liệu) tùy chọn setup về client(trả về cái gì?)
 
-// console.log('PATH', path.join(__dirname, 'resources/views'))
-
-
-app.get('/trangchu', (req, res) => {
-  res.render('home');
+app.get('/', (req, res) => {
+    res.render('home')
 })
-
 
 app.get('/news', (req, res) => {
-  res.render('news');
+  res.render('news')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
