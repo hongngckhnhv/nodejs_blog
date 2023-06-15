a03dc77cb625a88803709c14a40a15ad19372a4b
Khanh Vo Hoang Ngoc <vokhanh2002vl1@gmail.com>
2023-06-15 22:05:46 +0700

    routes & controllers ?

--- src/server.js
+++ src/server.js
@@ -4,7 +4,16 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express() // trả về đối tượng để xây dựng lại web 
 const port = 3000
+
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 
 // Using combined predefined format, http logger
 app.use(morgan("combined"));
@@ -18,13 +27,8 @@ app.set('views', path.join(__dirname, 'resources/views'))
 //req = request : chứa tất cả thông tin yêu cầu gửi đi(path, đường dẫn, phương thức, thông tin liên quan trong header) yêu cầu về phía server
 //res = response : server nhận request -> xử lý (trả về client các dữ liệu) tùy chọn setup về client(trả về cái gì?)
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+//Routes init  (khởi tạo tuyến đường)
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
