4bdb18a445c0b6a7192a28444333b0dabdce6383
Khanh Vo Hoang Ngoc <vokhanh2002vl1@gmail.com>
2023-07-05 21:12:36 +0700

    update delete count but error =(((

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,13 +3,28 @@ const {multipleMongooseToObject} = require('../../util/mongoose')
 
 
 class MeController {
-    //[GET] /me/stored/courses
+    // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: multipleMongooseToObject(courses)
-            }))
-            .catch(next)
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next);
+
+        // Course.countDocumentDeleted()
+        //     .then((deleteCount) =>{
+
+        //     })
+        //     .catch(()=> {})
+
+        // Course.find({})
+        //     .then(courses => res.render('me/stored-courses', {
+        //         courses: multipleMongooseToObject(courses)
+        //     }))
+        //     .catch(next)
     }
     //[GET] /me/trash/courses
     trashCourses(req,res,next){
